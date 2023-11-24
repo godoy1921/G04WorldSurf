37bf536d4ca082f5761270f54019e998669b87b8
godoy1921 <iker.godoy@opendeusto.es>
2023-11-24 14:08:30 +0100

    th

---
+++ SurfWorld/src/data/cargaSurfista.java
@@ -0,0 +1,44 @@
+package data;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import domain.Surfista;
+
+
+
+public class cargaSurfista {
+    public static void main(String[] args) {
+        String csvFile = "resources\\surfistas.csv";
+        String line = "";
+        String cvsSplitBy = ",";
+
+        ArrayList<Surfista> surfistas = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+        	
+        	br.readLine();
+        	
+            while ((line = br.readLine()) != null) {
+                String[] datos = line.split(cvsSplitBy);
+                int id = Integer.parseInt(datos[0]);
+                String nombre = datos[1];
+                String pais = datos[2];
+                int ranking = Integer.parseInt(datos[3]);
+
+                // Crear objeto Surfista y agregarlo a la lista
+                Surfista surfista = new Surfista(id, nombre, pais, ranking);
+                surfistas.add(surfista);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Imprimir la lista de surfistas creados
+        for (Surfista surfista : surfistas) {
+            System.out.println(surfista);
+        }
+    }
+}
