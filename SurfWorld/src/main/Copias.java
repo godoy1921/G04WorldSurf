eeacab6507f55ada6f41ec885e4d3cccbb81cb48
godoy1921 <iker.godoy@opendeusto.es>
2023-12-31 12:54:58 +0100

    simulacion torneo imposible

--- SurfWorld/src/main/Copias.java
+++ SurfWorld/src/main/Copias.java
@@ -1,315 +1,57 @@
 package main;
 
-import java.net.URL;
 import javax.swing.*;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellRenderer;
-
-import data.cargaEvento;
-import domain.Evento;
-import domain.Surfista;
-
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.YearMonth;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
 
-public class Copias {
-	private static DefaultTableModel calendarModel;
-    private static DefaultTableModel eventsModel;
-    private static JTable calendarTable;
-    private static JTable eventsTable;
-    private static LocalDate selectedDate;
+public class Copias extends JFrame {
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Calendario y Eventos");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
+    private static final int PARTICIPANTES = 16;
+    private static final int NIVELES = 4;
+    private static final int LADO = 50;
 
-        JPanel calendarPanel = new JPanel(new BorderLayout());
-        JPanel eventsPanel = new JPanel(new BorderLayout());
+    public Copias() {
+        setTitle("Torneo de Surf");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
 
-        calendarModel = new DefaultTableModel(6, 7);
-        String[] daysOfWeek = {"Lun", "Mar", "Mié", "Jue", "Vie", "Sáb", "Dom"};
-        calendarModel.setColumnIdentifiers(daysOfWeek);
-
-        calendarTable = new JTable(calendarModel) {
+        JPanel panel = new JPanel() {
             @Override
-            public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
-                Component comp = super.prepareRenderer(renderer, row, column);
-                comp.setBackground(Color.WHITE);
-                Object value = getModel().getValueAt(row, column);
-                //if (value != null && value.equals(selectedDate.getDayOfMonth())) {
-                    //comp.setBackground(Color.CYAN);
-                    
-                //}
-                return comp;
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+
+                int nivelHeight = getHeight() / (NIVELES + 1);
+                int nivelWidth = getWidth() / (PARTICIPANTES / 2 + 1);
+
+                for (int nivel = 0; nivel < NIVELES; nivel++) {
+                    int y = (nivel + 1) * nivelHeight;
+
+                    for (int i = 0; i < Math.pow(2, nivel) && i * 2 + 1 < PARTICIPANTES; i++) {
+                        int x = (i + 1) * nivelWidth;
+
+                        g.drawRect(x - LADO / 2, y - LADO / 2, LADO, LADO);
+
+                        int childY = (nivel + 2) * nivelHeight;
+                        int childX1 = (i * 2 + 1) * nivelWidth;
+                        int childX2 = (i * 2 + 2) * nivelWidth;
+
+                        g.drawLine(x, y, childX1, childY);
+                        g.drawLine(x, y, childX2, childY);
+                    }
+                }
             }
         };
-        
-        calendarTable.setRowHeight(60); // Ajusta la altura de las filas
-        calendarTable.getColumnModel().getColumn(0).setPreferredWidth(80); // Ajusta el ancho de las celdas
 
-        calendarTable.setIntercellSpacing(new Dimension(5, 5));
-        calendarTable.setDefaultEditor(Object.class, null);
-
-        JScrollPane calendarScrollPane = new JScrollPane(calendarTable);
-        calendarPanel.add(createNavigationPanel(), BorderLayout.NORTH);
-        calendarPanel.add(calendarScrollPane, BorderLayout.CENTER);
-
-        eventsModel = new DefaultTableModel(new String[]{"Evento", "Surfistas"}, 0);
-        eventsTable = new JTable(eventsModel);
-        JScrollPane eventsScrollPane = new JScrollPane(eventsTable);
-        eventsPanel.add(eventsScrollPane, BorderLayout.CENTER);
-        
-
-
-        JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, calendarPanel, eventsPanel);
-        splitPane.setDividerLocation(350); // Ajusta la posición del divisor
-        frame.add(splitPane);
-        
-        IconRenderer iconRenderer = new IconRenderer();
-        calendarTable.setDefaultRenderer(Object.class, iconRenderer);
-
-
-        frame.setVisible(true);
-        LocalDate now = LocalDate.now();
-        selectedDate = now;
-        updateCalendar(now.getMonthValue(), now.getYear());
-        cargarInformacionEventos(now);
+        panel.setPreferredSize(new Dimension(800, 500));
+        getContentPane().add(panel);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 
-    private static JPanel createNavigationPanel() {
-        JPanel panel = new JPanel();
-
-        String[] monthNames = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
-                "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
-        JComboBox<String> monthComboBox = new JComboBox<>(monthNames);
-
-        JComboBox<String> yearComboBox = new JComboBox<>();
-        int year = LocalDate.now().getYear();
-        for (int i = year - 1; i <= year + 1; i++) {
-            yearComboBox.addItem(String.valueOf(i));
-        }
-
-        panel.add(monthComboBox);
-        panel.add(yearComboBox);
-        
-        monthComboBox.setSelectedIndex(LocalDate.now().getMonthValue() - 1);
-        yearComboBox.setSelectedItem(String.valueOf(LocalDate.now().getYear()));
-
-        
-        
-     // En el ActionListener de los ComboBox de mes y año
-        monthComboBox.addActionListener(e -> {
-            int selectedMonth = monthComboBox.getSelectedIndex() + 1;
-            int selectedYear = Integer.parseInt((String) yearComboBox.getSelectedItem());
-            updateCalendar(selectedMonth, selectedYear);
-            LocalDate selectedDate = LocalDate.of(selectedYear, selectedMonth, 1);
-            cargarInformacionEventos(selectedDate);
-        });
-
-        yearComboBox.addActionListener(e -> {
-            int selectedMonth = monthComboBox.getSelectedIndex() + 1;
-            int selectedYear = Integer.parseInt((String) yearComboBox.getSelectedItem());
-            updateCalendar(selectedMonth, selectedYear);
-            LocalDate selectedDate = LocalDate.of(selectedYear, selectedMonth, 1);
-            cargarInformacionEventos(selectedDate);
-        });
-
-        
-
-        return panel;
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Copias::new);
     }
-
-    private static void cargarInformacionEventos(LocalDate selectedDate) {
-        // Obtener eventos del archivo de datos o de donde sea que los obtengas
-        List<Evento> eventos = cargaEvento.cargarEventos();
-
-        // Limpiar la tabla de eventos
-        eventsModel.setRowCount(0);
-
-        // Iterar sobre los eventos y agregar los correspondientes a la fecha seleccionada
-        for (Evento evento : eventos) {
-            // Convertir las fechas de String a LocalDate (usando métodos parseados que tengas)
-            LocalDate fechaInicio = parsearFecha(evento.getFechaInicio());
-            LocalDate fechaFin = parsearFecha(evento.getFechaFin());
-           
-            // Verificar si el evento cae en el día seleccionado
-            if (((selectedDate.getMonth().getValue() > fechaInicio.getMonth().getValue() && selectedDate.getMonth().getValue() < fechaFin.getMonth().getValue()) 
-            		|| (selectedDate.getMonth().getValue() == fechaInicio.getMonth().getValue()) || (selectedDate.getMonth().getValue() == fechaFin.getMonth().getValue()))
-            		&& (selectedDate.getYear() == fechaInicio.getYear() || selectedDate.getYear() == fechaFin.getYear())) {
-                // Obtener los surfistas participantes y agregarlos a la tabla de eventos
-                StringBuilder surfistas = new StringBuilder();
-                for (Surfista surfista : evento.getParticipantes()) {
-                    surfistas.append(surfista.getNombre()).append(", ");
-                }
-                eventsModel.addRow(new Object[]{evento.getNombre(), surfistas.toString()});
-            }
-        }
-    }
-
-    // Método para convertir String a LocalDate (aquí necesitas implementar tu lógica de conversión)
-    private static LocalDate parsearFecha(String fecha) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        return LocalDate.parse(fecha, formatter);
-    }
+}
 
 
-    private static void updateCalendar(int selectedMonth, int selectedYear) {
-        calendarModel.setRowCount(0);
-
-        selectedDate = LocalDate.of(selectedYear, selectedMonth, 1);
-        YearMonth yearMonth = YearMonth.of(selectedYear, selectedMonth);
-
-        int firstDayOfWeek = selectedDate.getDayOfWeek().getValue();
-        int daysInMonth = yearMonth.lengthOfMonth();
-        int row = 0;
-        int day = 1;
-
-        for (int i = 0; i < 6; i++) {
-            calendarModel.addRow(new Object[7]);
-        }
-
-        for (int i = 1; i < firstDayOfWeek; i++) {
-            calendarModel.setValueAt("", 0, i - 1);
-        }
-
-        for (int i = firstDayOfWeek; i < 8; i++) {
-            calendarModel.setValueAt(day++, 0, i - 1);
-        }
-
-        for (int i = 1; i < 6; i++) {
-            for (int j = 0; j < 7 && day <= daysInMonth; j++) {
-                calendarModel.setValueAt(day++, i, j);
-            }
-        }
-        
-        List<Evento> eventos = cargaEvento.cargarEventos();
-        
-     // Obtener el día de la semana en que comienza el mes y calcular el offset
-        int firstDayOfWeekX = selectedDate.withDayOfMonth(1).getDayOfWeek().getValue();
-        int offset = (firstDayOfWeekX + 5) % 7; // Calcula el desplazamiento para empezar desde el domingo
-
-        // Iterar sobre los eventos y colocar los íconos en las celdas correspondientes
-        for (Evento evento : eventos) {
-            LocalDate fechaInicio = parsearFecha(evento.getFechaInicio());
-            LocalDate fechaFin = parsearFecha(evento.getFechaFin());
-
-            // Iterar sobre el rango de fechas del evento
-            for (LocalDate date = fechaInicio; date.isBefore(fechaFin.plusDays(1)); date = date.plusDays(1)) {
-                if (date.getMonthValue() == selectedMonth && date.getYear() == selectedYear) {
-                    int dayOfMonth = date.getDayOfMonth();
-
-                    // Calcular la posición de la celda para colocar el ícono
-                    int rowX = (dayOfMonth + offset) / 7;
-                    int column = (dayOfMonth + offset) % 7;
-
-                    // Asegurarse de que el ícono se coloque en la celda del día 1 del mes
-                    if (dayOfMonth == 1) {
-                        rowX = 0;
-                        column = (firstDayOfWeekX + 6) % 7; // Ajustar columna para el día 1
-                        
-                    }
-
-                    ImageIcon iconoEvento = obtenerIconoEvento(evento); 
-                    calendarTable.setValueAt(iconoEvento, rowX, column);
-                }
-            }
-        }
-        
-
-        calendarTable.repaint();
-    }
-    
-    private static ImageIcon obtenerIconoEvento(Evento evento) {
-        String nombreEvento = evento.getNombre();
-        String nombreImagen = "";
-
-        switch (nombreEvento) {
-            case "World Surf League":
-                nombreImagen = "WorldSurfLeague.png";
-                break;
-            case "Billabong Pipe Masters":
-                nombreImagen = "Billabong.png";
-                break;
-            case "Jbay Open":
-                nombreImagen = "JbayOpen.jpg";
-                break;
-            case "Margaret River Pro":
-                nombreImagen = "MargaretRiver.jpg";
-                break;
-            case "Quick Silver Pro":
-                nombreImagen = "QuickSilver.jpg";
-                break;
-            case "Tahiti Pro":
-                nombreImagen = "TahitiPro.jpg";
-                break;
-            default:
-                // Nombre de imagen predeterminado o manejo de error si no se encuentra el evento
-                nombreImagen = "default.png";
-                break;
-        }
-        
-      
-
-        // Suponiendo que los logos están en una carpeta llamada "logos" dentro del proyecto
-        String rutaImagen = "surfworld/img/" + nombreImagen;
-        ImageIcon icono = new ImageIcon(rutaImagen);
-       
-
-        return icono;
-    }
-    
-    static class IconRenderer extends DefaultTableCellRenderer {
-        private int iconWidth = 50; // Ancho deseado de la imagen
-        private int iconHeight = 50; // Alto deseado de la imagen
-        
-        public IconRenderer() {
-            setHorizontalAlignment(SwingConstants.CENTER); // Centra los elementos en la celda
-            setVerticalAlignment(SwingConstants.CENTER);
-        }
-
-        @Override
-        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-            JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-            label.setIcon(null); // Limpia el icono
-
-            if (value instanceof ImageIcon) {
-                ImageIcon originalIcon = (ImageIcon) value;
-                Image img = originalIcon.getImage();
-                Image newImg = img.getScaledInstance(iconWidth, iconHeight, Image.SCALE_SMOOTH);
-                ImageIcon scaledIcon = new ImageIcon(newImg);
-                label.setIcon(scaledIcon);
-                label.setText(""); // Borra el texto
-            } 
-            return label;
-        }
-    }
-    
-
-
-
-
-    }
-
-
-
-
-
-	
-
-
-
-	
-	
 
--- SurfWorld/src/main/Copias2.java
+++ SurfWorld/src/main/Copias2.java
@@ -1,104 +1,59 @@
 package main;
 
-
-
 import javax.swing.*;
-import javax.swing.event.*;
-import javax.swing.table.DefaultTableModel;
-
-import com.toedter.calendar.JCalendar;
-
-import data.cargaEvento;
-import domain.Evento;
-import domain.Surfista;
-
 import java.awt.*;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
 
-public class Copias2 {
-    private DefaultTableModel tableModel;
+public class Copias extends JFrame {
 
-    public Copias2() {
-        JFrame frame = new JFrame("Calendario de Eventos");
-        frame.setSize(800, 600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    private static final int PARTICIPANTES = 16;
+    private static final int NIVELES = 4;
+    private static final int LADO = 50;
 
-        JCalendar calendar = new JCalendar();
-        JPanel panel = new JPanel(new BorderLayout());
-        panel.add(calendar, BorderLayout.CENTER);
-        calendar.setTodayButtonVisible(true);
-        calendar.setTodayButtonText("Recent");
-        calendar.setWeekOfYearVisible(false);
+    public Copias() {
+        setTitle("Torneo de Surf");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
 
-        // Crear el JTable para mostrar la información del evento y los surfistas
-        tableModel = new DefaultTableModel(new String[]{"Evento", "Surfistas"}, 0);
-        JTable table = new JTable(tableModel);
-        JScrollPane scrollPane = new JScrollPane(table);
-        panel.add(scrollPane, BorderLayout.SOUTH);
+        JPanel panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
 
-        // Agregar listener al JCalendar para detectar la selección de fecha
-        calendar.getDayChooser().addPropertyChangeListener(new PropertyChangeListener() {
-            public void propertyChange(PropertyChangeEvent e) {
-                if (e.getPropertyName().equals("day")) {
-                    Date selectedDate = calendar.getDate(); // Obtener la fecha del calendario
-                    cargarInformacionEventos(selectedDate);
+                int[] posX = new int[PARTICIPANTES + 1];
+                int nivelHeight = getHeight() / (NIVELES + 2);
+
+                for (int nivel = 0; nivel <= NIVELES; nivel++) {
+                    int nodeCount = (int) Math.pow(2, nivel);
+                    int nivelWidth = getWidth() / (nodeCount + 1);
+
+                    for (int i = 1; i <= nodeCount; i++) {
+                        int x = i * nivelWidth;
+                        int y = (nivel + 1) * nivelHeight;
+                        g.drawRect(x - LADO / 2, y - LADO / 2, LADO, LADO);
+                        posX[i] = x;
+                        if (nivel > 0 && i <= nodeCount / 2) {
+                            int childX1 = 2 * x - nivelWidth;
+                            int childX2 = 2 * x + nivelWidth;
+                            int childY = (nivel + 2) * nivelHeight;
+                            g.drawLine(x, y - LADO / 2, childX1, childY - LADO / 2);
+                            g.drawLine(x, y - LADO / 2, childX2, childY - LADO / 2);
+                        }
+                    }
                 }
             }
-        });
+        };
 
-
-        frame.add(panel);
-        frame.setVisible(true);
-    }
-    
-
-
-    private void cargarInformacionEventos(java.util.Date selectedDate) {
-        // Obtener eventos del archivo de datos (reemplaza esto por tu lógica para obtener eventos)
-        List<Evento> eventos = cargaEvento.cargarEventos();
-
-        // Limpiar la tabla
-        tableModel.setRowCount(0);
-
-        // Buscar eventos para la fecha seleccionada
-        for (Evento evento : eventos) {
-            // Convertir las fechas de String a Date
-            java.util.Date fechaInicio = parsearFecha(evento.getFechaInicio());
-            java.util.Date fechaFin = parsearFecha(evento.getFechaFin());
-
-            if (selectedDate.after(fechaInicio) && selectedDate.before(fechaFin)) {
-                // Agregar evento y surfistas a la tabla
-            	StringBuilder surfistas = new StringBuilder();
-                for (Surfista surfista : evento.getParticipantes()) {
-                    surfistas.append(surfista.getNombre()).append(", ");
-                }
-                tableModel.addRow(new Object[]{evento.getNombre(), surfistas.toString()});
-                
-             
-            }
-        }
-    }
-
-    // Método para convertir String a Date
-    private java.util.Date parsearFecha(String fecha) {
-        // Implementa tu lógica para parsear la fecha
-        // Aquí se asume un formato de fecha dd-MM-yyyy
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
-        try {
-            return formatter.parse(fecha);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return null;
+        panel.setPreferredSize(new Dimension(800, 550));
+        getContentPane().add(panel);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 
     public static void main(String[] args) {
-        // Crear la ventana del calendario
-        SwingUtilities.invokeLater(() -> new Copias2());
+        SwingUtilities.invokeLater(Copias::new);
     }
 }
+
+
+
