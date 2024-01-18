d84e77faf632cb1b229d71c0026be063c41a6f54
godoy1921 <iker.godoy@opendeusto.es>
2024-01-18 15:24:43 +0100

    terimnado final enserio

--- SurfWorld/src/gui/VentanaInicio.java
+++ SurfWorld/src/gui/VentanaInicio.java
@@ -8,21 +8,6 @@ import data.cargaSurfista;
 import domain.Evento;
 import domain.Surfista;
 
-//public class ventanaInicio extends JFrame {
-    //public ventanaInicio() {
-        //JFrame frame = new JFrame("Ventana de Inicio");
-        //frame.setSize(600, 400);
-        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //JLabel label = new JLabel("Bienvenido a la aplicación de surf");
-        //JPanel panel = new JPanel();
-        //panel.add(label);
-        //frame.add(panel);
-
-        //frame.setVisible(true);
-    //}
-//}
-
 
 import javax.swing.*;
 import java.awt.*;
